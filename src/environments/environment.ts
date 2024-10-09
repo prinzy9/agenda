7a145151bf405a06ba7839449d42d2359cb4d7ac
Aleira <ale.secra@gmail.com>
2024-10-09 17:43:17 +0200

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiURL_RESOURCE: 'https://0bk0v0jh-57291.euw.devtunnels.ms/api/v1/GetInfoRisorse',
     apiURL_CDC: 'https://0bk0v0jh-57291.euw.devtunnels.ms/api/v1/GetCentriDiCosto',
-
+    apiURL_EVENTS: 'https://0bk0v0jh-57291.euw.devtunnels.ms/api/v1/GetEventi',
 };
